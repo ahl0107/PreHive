aaa7d4b68b770bddc35d403eb4ab8060d4539106
liaihong <>
2020-11-30 12:15:02 +0800

    no message

--- ElastosHiveSDK/VaultAuthHelper.swift
+++ ElastosHiveSDK/VaultAuthHelper.swift
@@ -193,6 +193,7 @@ public class VaultAuthHelper: ConnectHelper {
                           encoding: JSONEncoding.default,
                           headers: header).responseJSON()
         var responseJson = try VaultApi.handlerJsonResponse(response)
+        _ = try VaultApi.handlerJsonResponseCanRelogin(responseJson, tryAgain: 1)
         let challenge = responseJson["challenge"].stringValue
         let semaphore: DispatchSemaphore = DispatchSemaphore(value: 0)
         var err: String = ""
